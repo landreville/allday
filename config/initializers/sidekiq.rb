6dd68076f88547a321e328a16466a0a6933e4df8
Jason <jason@beanfield.com>
2026-03-01 14:42:12 -0500

    feat: configure Sidekiq for background job processing

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,7 @@
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV.fetch("REDIS_URL", "redis://localhost:6381/0") }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV.fetch("REDIS_URL", "redis://localhost:6381/0") }
+end
